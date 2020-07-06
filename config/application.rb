87ea7d10b05c77ac6acd127dd67400b37f31b32e
lokideos <archmage09@gmail.com>
2020-07-06 22:41:19 +0300

    Change root roda response

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,13 @@
 class Application < Roda
   plugin(:not_found) { { error: 'Not found' } }
+  plugin :environments
   plugin(:json_parser)
 
   route do |r|
     r.root do
-      '<h1>The RODA root</h1>'
+      response['Content-Type'] = 'application/json'
+      response.status = 200
+      JSON({ status: "ok" })
     end
   end
 end
