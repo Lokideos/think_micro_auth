0930e9a22df21f442ef2a000e5b34bda94dad0df
lokideos <archmage09@gmail.com>
2020-07-07 21:25:26 +0300

    Add ability to create new users

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,26 @@
 # frozen_string_literal: true
 
 class Application < Roda
+  plugin :error_handler do |e|
+    case e
+    when Sequel::NoMatchingRow
+      response.status = 422
+      error_response I18n.t(:not_found, scope: 'api.errors')
+    when Sequel::UniqueConstraintViolation
+      response.status = 422
+      error_response I18n.t(:not_unique, scope: 'api.errors')
+    when Validations::InvalidParams, KeyError
+      response.status = 422
+      error_response I18n.t(:missing_parameters, scope: 'api.errors')
+    else
+      raise
+    end
+  end
   plugin(:not_found) { { error: 'Not found' } }
   plugin :environments
   plugin(:json_parser)
+  include Validations
+  include ApiErrors
 
   route do |r|
     r.root do
@@ -12,8 +29,31 @@ class Application < Roda
       JSON({ status: 'ok' })
     end
 
+    r.on 'v1' do
+      r.on 'users' do
+        r.post do
+          user_params = validate_with!(UserParamsContract).to_h.values
+          result = Users::CreateService.call(*user_params)
+          response['Content-Type'] = 'application/json'
+
+          if result.success?
+            response.status = 201
+            JSON({ status: 'created' })
+          else
+            error_response(result.user)
+          end
+        end
+      end
+    end
+
     r.get 'favicon.ico' do
       'no icon'
     end
+  end
+
+  private
+
+  def params
+    request.params
   end
 end
