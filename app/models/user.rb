56bd149629ac582fcddcc897328659ee3a9d82ea
lokideos <archmage09@gmail.com>
2020-07-07 19:01:50 +0300

    Add user model and migration

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class User < Sequel::Model
+  NAME_FORMAT = /\A\w+\z/.freeze
+
+  def validate
+    super
+    validates_presence :name, message: I18n.t(:blank, scope: 'model.errors.user.name')
+    validates_presence :email, message: I18n.t(:blank, scope: 'model.errors.user.email')
+    validates_format NAME_FORMAT, :name, message: I18n.t(:format, scope: 'model.errors.user.name')
+  end
+end
