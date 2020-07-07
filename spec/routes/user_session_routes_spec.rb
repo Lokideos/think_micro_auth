3fed4571511b3c0ee87b4b0b39cab9d864e52d84
lokideos <archmage09@gmail.com>
2020-07-07 23:19:53 +0300

    Add tests for session routes

---
+++ spec/routes/user_session_routes_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+RSpec.describe Application, type: :routes do
+  describe 'POST /login' do
+    context 'missing parameters' do
+      let(:params) { { email: 'bob@example.com', password: '' } }
+
+      it 'returns an error' do
+        post 'v1/sessions', params
+
+        expect(last_response.status).to eq(401)
+      end
+    end
+
+    context 'invalid parameters' do
+      let(:params) { { email: 'bob@example.com', password: 'invalid' } }
+
+      it 'returns an error' do
+        post 'v1/sessions', params
+
+        expect(last_response.status).to eq(401)
+        expect(response_body['errors']).to include('detail' => 'Сессия не может быть создана')
+      end
+    end
+
+    context 'valid parameters' do
+      let(:token) { 'jwt_token' }
+      let(:params) { { email: 'bob@example.com', password: 'givemeatoken' } }
+
+      before do
+        Fabricate(:user, email: 'bob@example.com', password: 'givemeatoken')
+
+        allow(JWT).to receive(:encode).and_return(token)
+      end
+
+      it 'returns created status' do
+        post 'v1/sessions', params
+
+        expect(last_response.status).to eq(201)
+        expect(response_body['meta']).to eq('token' => token)
+      end
+    end
+  end
+end
