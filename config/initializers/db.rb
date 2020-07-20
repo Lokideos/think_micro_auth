991f7511a92cb38e90e8617241b9efb39ad63355
lokideos <archmage09@gmail.com>
2020-07-20 17:01:38 +0300

    Delete comment

--- config/initializers/db.rb
+++ config/initializers/db.rb
@@ -8,7 +8,6 @@ Sequel.connect(Settings.db.to_hash)
 Sequel::Model.db.extension(:pagination)
 
 Sequel::Model.plugin :validation_helpers
-# Sequel::Model.plugin :secure_password
 Sequel::Model.plugin :timestamps, update_on_create: true
 
 Sequel.default_timezone = :utc
