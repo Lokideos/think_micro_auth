aaead4ce8bb1309260caf05f384e94c81dc1ff56
lokideos <archmage09@gmail.com>
2020-07-20 14:54:23 +0300

    Use association_dependencies plugin for add delete user sessions with user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
 class User < Sequel::Model
+  plugin :association_dependencies
   plugin :secure_password, include_validations: false
 
   NAME_FORMAT = /\A\w+\z/.freeze
 
   one_to_many :sessions, class: 'UserSession'
 
+  add_association_dependencies sessions: :delete
+
   def validate
     super
+
     validates_presence :name, message: I18n.t(:blank, scope: 'model.errors.user.name')
     validates_presence :email, message: I18n.t(:blank, scope: 'model.errors.user.email')
     if new?
