f8a5a329a7000643c005bbeeb1cc4f742b8d5a8e
lokideos <archmage09@gmail.com>
2020-07-23 13:27:30 +0300

    Add authentication via rpc

--- app/helpers/auth.rb
+++ app/helpers/auth.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 
 module Auth
+  class UnknownTokenMode < StandardError; end
+
   AUTH_TOKEN = /\ABearer (?<token>.+)\z/.freeze
 
-  def extracted_token
-    JwtEncoder.decode(matched_token)
+  def extracted_token(token_mode: :http, rpc_token: nil)
+    case token_mode
+    when :http then JwtEncoder.decode(matched_token)
+    when :rpc then JwtEncoder.decode(rpc_token)
+    else
+      raise UnknownTokenMode
+    end
   rescue JWT::DecodeError
     {}
   end
