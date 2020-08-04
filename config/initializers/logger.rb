b3eb7129f42d16b125cd86d9bdfa60c4e379dae9
lokideos <archmage09@gmail.com>
2020-08-04 14:53:41 +0300

    Comment sections for debug

--- config/initializers/logger.rb
+++ config/initializers/logger.rb
@@ -1,14 +1,20 @@
 # frozen_string_literal: true
 
-if Application.environment == :development
-  logger = Ougai::Logger.new(STDOUT, level: Settings.logger.level)
-  logger.formatter = Ougai::Formatters::Readable.new
-else
-  logger = Ougai::Logger.new(
-    Application.root.concat('/', Settings.logger.path),
-    level: Settings.logger.level
-  )
-end
+# For proper debug
+# if Application.environment == :development
+#   logger = Ougai::Logger.new(STDOUT, level: Settings.logger.level)
+#   logger.formatter = Ougai::Formatters::Readable.new
+# else
+#   logger = Ougai::Logger.new(
+#     Application.root.concat('/', Settings.logger.path),
+#     level: Settings.logger.level
+#   )
+# end
+
+logger = Ougai::Logger.new(
+  Application.root.concat('/', Settings.logger.path),
+  level: Settings.logger.level
+)
 logger.before_log = lambda do |data|
   data[:service] = { name: Settings.app.name }
   data[:request_id] ||= Thread.current[:request_id]
